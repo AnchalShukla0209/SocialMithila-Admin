a06c286f8c40ce2f8483acbc22ab7a661ac09a78
Anchal <anchalshukla7060153412@gmail.com>
2025-11-12 23:01:10 +0530

    ok

--- SocialMithila/Controllers/HomeController.cs
+++ SocialMithila/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +11,71 @@ namespace SocialMithila.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Contact()
         {
-            ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public JsonResult GetUserList(string userIds = null, DateTime? fromDate = null, DateTime? toDate = null, string status = null, int pageIndex = 1, int pageSize = 10)
         {
-            ViewBag.Message = "Your contact page.";
+            List<object> users = new List<object>();
+            int totalCount = 0;
 
-            return View();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("USP_GetUserList", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UserIds", (object)userIds ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@FromDate", (object)fromDate ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@ToDate", (object)toDate ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Status", (object)status ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@PageIndex", pageIndex);
+                    cmd.Parameters.AddWithValue("@PageSize", pageSize);
+
+                    con.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            string photo = dr["ProfilePhoto"] == DBNull.Value || string.IsNullOrWhiteSpace(dr["ProfilePhoto"].ToString())
+                                ? "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                                : "https://socialmithila.com/" + dr["ProfilePhoto"].ToString();
+
+                            users.Add(new
+                            {
+                                Id = dr["Id"],
+                                FullName = $"{dr["FirstName"]} {dr["LastName"]}".Trim(),
+                                Email = dr["Email"],
+                                Phone = dr["Phone"],
+                                Country = dr["Country"],
+                                Address = dr["Address"],
+                                TownCity = dr["TownCity"],
+                                PostCode = dr["PostCode"],
+                                Description = dr["Description"],
+                                ProfilePhoto = photo,
+                                IsActive = Convert.ToBoolean(dr["IsActive"]),
+                                CreatedOn = Convert.ToDateTime(dr["CreatedOn"]).ToString("yyyy-MM-dd HH:mm")
+                            });
+                        }
+
+                        if (dr.NextResult() && dr.Read())
+                        {
+                            totalCount = Convert.ToInt32(dr["TotalCount"]);
+                        }
+                    }
+                }
+            }
+
+            return Json(new { Data = users, TotalCount = totalCount }, JsonRequestBehavior.AllowGet);
         }
     }
 }
