66f01a9a7f7d49b52b3243975d0b3faab3be2359
Anchal <anchalshukla7060153412@gmail.com>
2025-11-14 21:49:37 +0530

    ok

---
+++ SocialMithila/Controllers/BusinessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialMithila.Controllers
+{
+    public class BusinessController : Controller
+    {
+        // GET: Business
+        public ActionResult CategoryMgmt()
+        {
+            return View();
+        }
+    }
+}
